6a820b211f1efb53e8d6c4bf92a5ab09f8079218
esref <ecagataydgn@gmail.com>
2024-12-01 21:07:11 +0300

    customer-service: change customer id from long to string

--- customer-service/src/main/java/dev/ecagataydogan/customerservice/entity/Customer.java
+++ customer-service/src/main/java/dev/ecagataydogan/customerservice/entity/Customer.java
@@ -15,7 +15,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Customer {
 
     @Id
-    private Long id;
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
