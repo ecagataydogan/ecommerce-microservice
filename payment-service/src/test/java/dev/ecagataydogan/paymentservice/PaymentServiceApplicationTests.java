83bb6fb5b62738ef790fd455b9e9a347645f26ad
esref <ecagataydgn@gmail.com>
2024-12-02 23:32:24 +0300

    payment-service: inital setup

---
+++ payment-service/src/test/java/dev/ecagataydogan/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ecagataydogan.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
